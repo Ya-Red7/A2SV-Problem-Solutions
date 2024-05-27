2d3de93a16b089d72007f72e94ee772f7be8feef
Ya-Red7 <149231087+Ya-Red7@users.noreply.github.com>
2024-05-27 19:41:21 +0300

    Create Max Number of K-Sum Pairs.py

---
+++ Max Number of K-Sum Pairs.py
@@ -0,0 +1,14 @@
+def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l=ans=0
+        r=len(nums)-1
+        while l<r:
+            if nums[l]+nums[r]==k:
+                ans+=1
+                l+=1
+                r-=1
+            elif nums[l]+nums[r]>k:
+                r-=1
+            else:
+                l+=1
+        return ans
