9ae7272aee4c5b70d1359a45a775d6184841bb58
Ya-Red7 <149231087+Ya-Red7@users.noreply.github.com>
2024-07-13 16:50:19 +0300

    Create Next Greater Element ll.py

---
+++ Next Greater Element ll.py
@@ -0,0 +1,11 @@
+def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        stack = []
+        ans = [-1]*n
+        for i in range(n*2):
+            while stack and nums[stack[-1]]<nums[i%n]:
+                ans[stack.pop()]=nums[i%n]
+            if i<n:
+                stack.append(i)
+        
+        return ans
